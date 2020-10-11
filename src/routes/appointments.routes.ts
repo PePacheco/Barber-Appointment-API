94e967aa5847db15533f6be635b327803bc2f21b
PePacheco <pedro_pacheco98@outlook.com>
2020-10-11 20:28:44 -0300

    Adição de um repositorio de agendamentos

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,29 +1,25 @@
 import { Router } from 'express';
-import { startOfHour, parseISO, isEqual } from 'date-fns';
-import Appointment from '../models/Appointment'
+import { startOfHour, parseISO } from 'date-fns';
+import AppointmentsRepository from '../repositories/AppointmentsRepository';
 
 const appointmentRouter = Router();
+const appointmentsRepository = new AppointmentsRepository();
 
 // Route : http://localhost:3333/appointments
 
-const appointments: Appointment[] = [];
 
 appointmentRouter.post('/', (request, response) => {
-    console.log(appointments);
     const { provider, date } = request.body;
     
     const parsedDate = startOfHour(parseISO(date));
 
-    const findAppointmentInSameDate = appointments.find(appointment => 
-        isEqual(parsedDate, appointment.date)
-    );
+    const findAppointmentInSameDate = appointmentsRepository.findByDate(parsedDate);
 
     if(findAppointmentInSameDate) {
         return response.status(400).json({ message: "This appointment is already booked" });
     }
 
-    const appointment = new Appointment(provider, parsedDate);
-    appointments.push(appointment);
+    const appointment = appointmentsRepository.create(provider, parsedDate);
 
     return response.json(appointment);
 });
