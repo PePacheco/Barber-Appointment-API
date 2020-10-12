0923e0666e96e35bbc506a5eb0c93e051a1c6e79
PePacheco <pedro_pacheco98@outlook.com>
2020-10-12 15:42:13 -0300

    Criação de serviço para realizar agendamentos

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,27 +1,32 @@
 import { Router } from 'express';
-import { startOfHour, parseISO } from 'date-fns';
+import { parseISO } from 'date-fns';
 import AppointmentsRepository from '../repositories/AppointmentsRepository';
+import CreateAppointmentService from '../services/CreateAppointmentService';
 
 const appointmentRouter = Router();
 const appointmentsRepository = new AppointmentsRepository();
 
 // Route : http://localhost:3333/appointments
 
+appointmentRouter.get('/', (request, response) => {
+    return response.json(appointmentsRepository.all());
+});
 
 appointmentRouter.post('/', (request, response) => {
-    const { provider, date } = request.body;
-    
-    const parsedDate = startOfHour(parseISO(date));
+    try {
+        const { provider, date } = request.body;
 
-    const findAppointmentInSameDate = appointmentsRepository.findByDate(parsedDate);
+        const parsedDate = parseISO(date);
 
-    if(findAppointmentInSameDate) {
-        return response.status(400).json({ message: "This appointment is already booked" });
+        const createAppointment = new CreateAppointmentService(appointmentsRepository);
+        
+        const appointment = createAppointment.execute({ provider, date: parsedDate });
+
+        return response.json(appointment);
+
+    } catch(err) {
+        return response.status(400).json({ error: err.message });
     }
-
-    const appointment = appointmentsRepository.create(provider, parsedDate);
-
-    return response.json(appointment);
 });
 
 export default appointmentRouter;
