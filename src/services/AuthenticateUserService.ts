4c72947847b99939af203573aa1c72e19a9eb351
PePacheco <pedro_pacheco98@outlook.com>
2020-10-15 12:10:52 -0300

    Criando as rotas de autenticação do usuario para logar, retornando um JWT

---
+++ src/services/AuthenticateUserService.ts
@@ -0,0 +1,46 @@
+import { getRepository } from 'typeorm';
+import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
+
+import User from '../models/User';
+
+interface Request {
+    email: string;
+    password: string;
+}
+
+interface Response {
+    user: User;
+    token: string;
+}
+
+class AuthenticateUserService {
+    public async execute({ email, password}: Request): Promise<Response>
+    {
+        const usersRepository = getRepository(User);
+
+        const user = await usersRepository.findOne({ where: { email } });
+
+        if(!user) {
+            throw Error('Incorrect email/password combination');
+        }
+
+        const passwordMatched = await compare(password, user.password);
+
+        if(!passwordMatched) {
+            throw Error('Incorrect email/password combination');
+        }
+
+        const token = sign({}, 'eb5006c63a730dead0e8e476743fbca9', {
+            subject: user.id,
+            expiresIn: '1d'
+        });
+
+        return {
+            user,
+            token
+        };
+    }
+}
+
+export default AuthenticateUserService;
