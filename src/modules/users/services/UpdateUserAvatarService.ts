8e173f9a0f4de71bfed21e142ae87d7a7d1801e8
PePacheco <pedro_pacheco98@outlook.com>
2020-10-20 14:07:48 -0300

    Adicionando provider para armazenar fazer upload de avatar no disco

--- src/modules/users/services/UpdateUserAvatarService.ts
+++ src/modules/users/services/UpdateUserAvatarService.ts
@@ -6,6 +6,7 @@ import { inject, injectable } from 'tsyringe';
 import IUsersRepository from '../repositories/IUsersRepository';
 import AppError from '@shared/errors/AppError';
 import User from '../infra/typeorm/entities/User';
+import IStorageProvider from '@shared/container/providers/StorageProvider/models/IStorageProvider';
 
 interface Request {
     userId: string;
@@ -17,6 +18,9 @@ class UpdateUserAvatarService {
     constructor(
         @inject('UsersRepository')
         private usersRepository: IUsersRepository,
+
+        @inject('StorageProvider')
+        private storageProvider: IStorageProvider,
     ) {}
     
     public async execute({ userId, avatarFilename }: Request) : Promise<User>{
@@ -27,13 +31,10 @@ class UpdateUserAvatarService {
         }
 
         if(user.avatar) {
-            const userAvatarFilePath = path.join(uploadConfig.directory, user.avatar);
-            const userAvatarFileExists = await fs.promises.stat(userAvatarFilePath);
-
-            if(userAvatarFileExists) {
-                await fs.promises.unlink(userAvatarFilePath);
-            }
+             await this.storageProvider.deleteFile(user.avatar);
         }
+
+        const fileName = await this.storageProvider.saveFile(avatarFilename);
 
         user.avatar = avatarFilename;
         await this.usersRepository.save(user);
