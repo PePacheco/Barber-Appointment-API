68813c75d519dc66b92089805a43b71ff6b3e6d9
PePacheco <pedro_pacheco98@outlook.com>
2020-11-04 13:05:27 -0300

    Adicionando templates de email

--- src/modules/users/services/SendForgotPasswordEmailService.ts
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -1,4 +1,5 @@
 import { inject, injectable } from 'tsyringe';
+import path from 'path';
 
 import IMailProvider from '@shared/container/providers/MailProvider/models/IMailProvider'
 import IUsersRepository from '../repositories/IUsersRepository';
@@ -31,6 +32,8 @@ class SendForgotPasswordEmailService {
 
         const { token } = await this.userTokensRepository.generate(user.id);
 
+        const forgotPasswordTemplate = path.resolve(__dirname, '..', 'views','forgot_password.hbs')
+
         await this.mailProvider.sendMail({
             to: {
                 name: user.name,
@@ -38,10 +41,11 @@ class SendForgotPasswordEmailService {
             },
             subject: '[GoBarber] Recuperação de senha',
             templateData: {
-                template: 'Olá {{name}} {{token}}',
+                file: forgotPasswordTemplate,
                 variables: {
                     name: user.name,
-                    token
+                    link: `http://localhost:3000/reset_password?token=${token}`,
+                    token: token
                 }
             }
         });
