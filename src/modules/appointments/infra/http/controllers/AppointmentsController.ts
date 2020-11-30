41dabe498404549c7b2c554de8bcb17533b10d72
PePacheco <pedro_pacheco98@outlook.com>
2020-11-30 20:01:28 -0300

    Pequenos ajustes pontuais

--- src/modules/appointments/infra/http/controllers/AppointmentsController.ts
+++ src/modules/appointments/infra/http/controllers/AppointmentsController.ts
@@ -1,5 +1,4 @@
 import { Request, Response } from 'express';
-import { parseISO } from 'date-fns';
 import { container } from 'tsyringe';
 
 import CreateAppointmentService from '@modules/appointments/services/CreateAppointmentService';
@@ -8,7 +7,7 @@ export default class AppointmentsController {
     public async create(request: Request, response: Response): Promise<Response> {
         const { provider_id, date } = request.body;
 
-        const parsedDate = parseISO(date);
+        const parsedDate = new Date(date);
         
         const createAppointment = container.resolve(CreateAppointmentService);
         
